2ea5190033884dd0ed8c5345e58a9d509094c048
Divesh Kumar <>
2021-06-09 18:21:26 +0530

    Added new route for Bflex results.

--- docroot/modules/custom/verathon_bflex_calculator/src/Form/VerathonBflexCalculatorForm.php
+++ docroot/modules/custom/verathon_bflex_calculator/src/Form/VerathonBflexCalculatorForm.php
@@ -25,82 +25,82 @@ class VerathonBflexCalculatorForm extends FormBase
   public function buildForm(array $form, FormStateInterface $form_state)
   {
     $config = \Drupal::service('config.factory')->getEditable('verathon_bflex_calculator.settings')->get();
+    $values = $form_state->getValues();
+    // dump($values);
+    $form['#submitted'] = false;
+    // check if form has been submitted.
+    if (!empty($values)) {
+      $form['#submitted'] =  true;
+      $form['#form_values'] = $values;
+    } else {
+      $form['facility_name'] = [
+        '#type' => 'textfield',
+        '#required' => true,
+      ];
 
-    $form['facility_name'] = [
-      '#type' => 'textfield',
-      '#required' => TRUE,
-    ];
+      $form['total_annual_bronchoscopy_procedures'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
+      $form['procedures_count_single_usage'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
+      $form['total_annual_bronchoscopy_procedures'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
+      $form['your_bronchoscope_price'] = [
+        '#type' => 'number',
+      ];
 
-    $form['total_annual_bronchoscopy_procedures'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
-    $form['procedures_count_single_usage'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
-    $form['total_annual_bronchoscopy_procedures'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
-    $form['your_bronchoscope_price'] = [
-      '#type' => 'number',
-      '#required' => TRUE,
-    ];
+      // Repair & Maintenance Form Fields.
+      $form['total_reusable_bronchoscopes'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
+      $form['annual_service_cost_per_bronchoscope'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
+      $form['annual_out_of_pocket_repair_cost'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+      ];
 
-    // Repair & Maintenance Form Fields.
-    $form['total_reusable_bronchoscopes'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
-    $form['annual_service_cost_per_bronchoscope'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
-    $form['annual_out_of_pocket_repair_cost'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
+      // Hidden Reprocessing Costs.
+      $form['reprocessing_costs'] = [
+        '#type' => 'range',
+        '#attributes' => [
+          'class' => ['slider'],
+        ],
+        '#required' => true,
+      ];
 
-    // Hidden Reprocessing Costs.
-    $form['reprocessing_costs'] = [
-      '#type' => 'range',
-      '#attributes' => [
-        'class' => ['slider'],
-      ],
-      '#required' => TRUE,
-    ];
+      $form['actions'] = [
+        '#type' => 'actions',
+      ];
 
-    $form['actions'] = [
-      '#type' => 'actions',
-    ];
-    dump($config);
-    die;
+
+      $form['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Send'),
+      ];
+    }
     $form['#attached']['library'][] = 'verathon_bflex_calculator/verathon_bflex_calculator';
     $form['#theme'] = 'form__verathon_bflex_calculator_verathon_bflex_calculator';
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Send'),
-    ];
-
     return $form;
   }
 
@@ -109,9 +109,9 @@ class VerathonBflexCalculatorForm extends FormBase
    */
   public function validateForm(array &$form, FormStateInterface $form_state)
   {
-    if (mb_strlen($form_state->getValue('message')) < 10) {
-      $form_state->setErrorByName('name', $this->t('Message should be at least 10 characters.'));
-    }
+    // if (mb_strlen($form_state->getValue('message')) < 10) {
+    //   $form_state->setErrorByName('name', $this->t('Message should be at least 10 characters.'));
+    // }
   }
 
   /**
