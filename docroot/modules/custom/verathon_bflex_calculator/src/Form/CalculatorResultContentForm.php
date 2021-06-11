6e5056f28062801d8da142216f6a2301077d95bb
Divesh Kumar <>
2021-06-11 13:07:26 +0530

    Adding new changes for the results form.

--- docroot/modules/custom/verathon_bflex_calculator/src/Form/CalculatorResultContentForm.php
+++ docroot/modules/custom/verathon_bflex_calculator/src/Form/CalculatorResultContentForm.php
@@ -33,10 +33,10 @@ class CalculatorResultContentForm extends ConfigFormBase
   public function buildForm(array $form, FormStateInterface $form_state)
   {
     // Getting Default configurations.
-    $config = $this->config('verathon_bflex_calculator.settings')->get();
-    $values = $form_state->getValues();
+    // $config = $this->config('verathon_bflex_calculator.settings')->get();
+    // $values = $form_state->getValues();
     // Global Form
-    $form['current'] = [
+    /* $form['current'] = [
       '#type' => 'details',
       '#open' => FALSE,
       '#collapsible' => TRUE,
@@ -48,6 +48,7 @@ class CalculatorResultContentForm extends ConfigFormBase
       '#collapsible' => TRUE,
       '#title' => $this->t('Bflex Cost')
     ];
+
     // Global Section
     $form['global'] = [
       '#type' => 'details',
@@ -107,6 +108,7 @@ class CalculatorResultContentForm extends ConfigFormBase
       '#collapsible' => TRUE,
       '#title' => $this->t('Preventable Infections')
     ];
+    // Gloabl Current Fields
     $form['current']['result_current_current_main_heading'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Heading - Label'),
@@ -323,9 +325,254 @@ class CalculatorResultContentForm extends ConfigFormBase
       ],
       '#default_value' => !empty($values['result_current_preventable_infections_costs_label']) ? $values['result_current_preventable_infections_costs_label'] : $config['result_current_preventable_infections_costs_label'],
     ];
+    // BFlex Section
+
+    // GEnerating fields for the current section.
+    $form['with']['bflex_bronchoscope_usage'] = [
+      '#type' => 'details',
+      '#open' => FALSE,
+      '#collapsible' => TRUE,
+      '#title' => $this->t('Current bronchoscope usage')
+    ];
+    $form['with']['bflex_repair_maintenance_usage'] = [
+      '#type' => 'details',
+      '#open' => FALSE,
+      '#collapsible' => TRUE,
+      '#title' => $this->t('Repair & Maintenance')
+    ];
+    $form['with']['bflex_reprocessing_costs'] = [
+      '#type' => 'details',
+      '#open' => FALSE,
+      '#collapsible' => TRUE,
+      '#title' => $this->t('Reprocessing Costs')
+    ];
+    $form['with']['bflex_preventable_infections'] = [
+      '#type' => 'details',
+      '#open' => FALSE,
+      '#collapsible' => TRUE,
+      '#title' => $this->t('Preventable Infections')
+    ];
+
+    // Global With Fields
+    $form['with']['bflex_current_current_main_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Heading - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('CURRENT OPERATING COSTS'),
+      ],
+      '#default_value' => !empty($values['bflex_current_current_main_heading']) ? $values['bflex_current_current_main_heading'] : $config['bflex_current_current_main_heading'],
+    ];
+    $form['with']['bflex_current_current_sub_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Sub-heading - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Using reusable bronchoscopes only'),
+      ],
+      '#default_value' => !empty($values['bflex_current_current_sub_heading']) ? $values['bflex_current_current_sub_heading'] : $config['bflex_current_current_sub_heading'],
+    ];
+    $form['with']['bflex_current_grand_total_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual Estimated Operating Costs - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual Estimated Operating Costs'),
+      ],
+      '#default_value' => !empty($values['bflex_current_grand_total_label']) ? $values['bflex_current_grand_total_label'] : $config['bflex_current_grand_total_label'],
+    ];
+    // Bronchoscope section fields
+    $form['with']['bflex_bronchoscope_usage']['result_with_bronchoscope_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section Heading'),
+      '#attributes' => [
+        'placeholder' => $this->t('Current bronchoscope usage'),
+      ],
+      '#default_value' => !empty($values['result_with_bronchoscope_heading']) ? $values['result_with_bronchoscope_heading'] : $config['result_with_bronchoscope_heading'],
+    ];
+    $form['with']['bflex_bronchoscope_usage']['result_with_single_usage_count_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Single Usage Bronchoscopes - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Current bronchoscope usage'),
+      ],
+      '#default_value' => !empty($values['result_with_single_usage_count_label']) ? $values['result_with_single_usage_count_label'] : $config['result_with_single_usage_count_label'],
+    ];
+    $form['with']['bflex_bronchoscope_usage']['result_with_bronchoscope_cost_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual Cost - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual Cost'),
+      ],
+      '#default_value' => !empty($values['result_with_bronchoscope_cost_label']) ? $values['result_with_bronchoscope_cost_label'] : $config['result_with_bronchoscope_cost_label'],
+    ];
+
+    // Repair & Maintenance Costs Section fields
+    $form['with']['bflex_repair_maintenance_usage']['result_with_repair_maintenance_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section Heading'),
+      '#attributes' => [
+        'placeholder' => $this->t('Repair and maintenance costs'),
+      ],
+      '#default_value' => !empty($values['result_with_repair_maintenance_heading']) ? $values['result_with_repair_maintenance_heading'] : $config['result_with_repair_maintenance_heading'],
+    ];
+    $form['with']['bflex_repair_maintenance_usage']['result_with_reusable_bronchoscope_usage_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Reusable bronchoscopes (QTY) - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Reusable bronchoscopes (QTY)'),
+      ],
+      '#default_value' => !empty($values['result_with_reusable_bronchoscope_usage_label']) ? $values['result_with_reusable_bronchoscope_usage_label'] : $config['result_with_reusable_bronchoscope_usage_label'],
+    ];
+    $form['with']['bflex_repair_maintenance_usage']['result_with_annual_cost_of_service_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual cost of service agreement per reusable bronchoscope - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual cost of service agreement per reusable bronchoscope'),
+      ],
+      '#default_value' => !empty($values['result_with_annual_cost_of_service_label']) ? $values['result_with_annual_cost_of_service_label'] : $config['result_with_annual_cost_of_service_label'],
+    ];
+    $form['with']['bflex_repair_maintenance_usage']['result_with_annual_oop_cost_of_service_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual out-of-pocket repair costs for all reusable bronchoscopes - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual out-of-pocket repair costs for all reusable bronchoscopes'),
+      ],
+      '#default_value' => !empty($values['result_with_annual_oop_cost_of_service_label']) ? $values['result_with_annual_oop_cost_of_service_label'] : $config['result_with_annual_oop_cost_of_service_label'],
+    ];
+    $form['with']['bflex_repair_maintenance_usage']['result_with_repair_maintenance_costs_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual Cost - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual Cost'),
+      ],
+      '#default_value' => !empty($values['result_with_repair_maintenance_costs_label']) ? $values['result_with_repair_maintenance_costs_label'] : $config['result_with_repair_maintenance_costs_label'],
+    ];
+
+    // Repair & Maintenance Costs Section fields
+    $form['with']['bflex_reprocessing_costs']['result_with_reprocessing_costs_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section Heading'),
+      '#attributes' => [
+        'placeholder' => $this->t('Reprocessing costs per use'),
+      ],
+      '#default_value' => !empty($values['result_with_reprocessing_costs_heading']) ? $values['result_with_reprocessing_costs_heading'] : $config['result_with_reprocessing_costs_heading'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_reprocessing_cost_left_column_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Reprocessing costs - column header'),
+      '#attributes' => [
+        'placeholder' => $this->t('Reprocessing costs'),
+      ],
+      '#default_value' => !empty($values['result_with_reprocessing_cost_left_column_heading']) ? $values['result_with_reprocessing_cost_left_column_heading'] : $config['result_with_reprocessing_cost_left_column_heading'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_reprocessing_cost_right_column_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Average - column header'),
+      '#attributes' => [
+        'placeholder' => $this->t('Average'),
+      ],
+      '#default_value' => !empty($values['result_with_reprocessing_cost_right_column_heading']) ? $values['result_with_reprocessing_cost_right_column_heading'] : $config['result_with_reprocessing_cost_right_column_heading'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_ppe_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('PPE for personnel - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('PPE for personnel'),
+      ],
+      '#default_value' => !empty($values['result_with_ppe_label']) ? $values['result_with_ppe_label'] : $config['result_with_ppe_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_bedside_precleaning_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Bedside precleaning - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Bedside precleaning'),
+      ],
+      '#default_value' => !empty($values['result_with_bedside_precleaning_label']) ? $values['result_with_bedside_precleaning_label'] : $config['result_with_bedside_precleaning_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_leak_testing_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Leak testing - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Leak testing'),
+      ],
+      '#default_value' => !empty($values['result_with_leak_testing_label']) ? $values['result_with_leak_testing_label'] : $config['result_with_leak_testing_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_manual_cleaning_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Manual Cleaning - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Manual cleaning'),
+      ],
+      '#default_value' => !empty($values['result_with_manual_cleaning_label']) ? $values['result_with_manual_cleaning_label'] : $config['result_with_manual_cleaning_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_visual_inspection_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Visual Inspection - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Visual Inspection'),
+      ],
+      '#default_value' => !empty($values['result_with_visual_inspection_label']) ? $values['result_with_visual_inspection_label'] : $config['result_with_visual_inspection_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_hld_in_aer_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('HLD in an AER - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('HLD in an AER'),
+      ],
+      '#default_value' => !empty($values['result_with_hld_in_aer_label']) ? $values['result_with_hld_in_aer_label'] : $config['result_with_hld_in_aer_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_dying_storage_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Dying and Storage - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Dying and Storage'),
+      ],
+      '#default_value' => !empty($values['result_with_dying_storage_label']) ? $values['result_with_dying_storage_label'] : $config['result_with_dying_storage_label'],
+    ];
+    $form['with']['bflex_reprocessing_costs']['result_with_reprocessing_costs_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual Cost - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual Cost'),
+      ],
+      '#default_value' => !empty($values['result_with_reprocessing_costs_label']) ? $values['result_with_reprocessing_costs_label'] : $config['result_with_reprocessing_costs_label'],
+    ];
+
+    // Preventable Infections Section Fields.
+    $form['with']['bflex_preventable_infections']['result_with_preventable_infections_heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section Heading - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Preventable Infections'),
+      ],
+      '#default_value' => !empty($values['result_with_preventable_infections_heading']) ? $values['result_with_preventable_infections_heading'] : $config['result_with_preventable_infections_heading'],
+    ];
+    $form['with']['bflex_preventable_infections']['result_with_preventable_infections_patient_infections_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Patient infections due to cross contamination - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Patient infections due to cross contamination'),
+      ],
+      '#default_value' => !empty($values['result_with_preventable_infections_patient_infections_label']) ? $values['result_with_preventable_infections_patient_infections_label'] : $config['result_with_preventable_infections_patient_infections_label'],
+    ];
+    $form['with']['bflex_preventable_infections']['result_with_cost_per_infection_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Cost per infection - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Cost per infection'),
+      ],
+      '#default_value' => !empty($values['result_with_cost_per_infection_label']) ? $values['result_with_cost_per_infection_label'] : $config['result_with_cost_per_infection_label'],
+    ];
+    $form['with']['bflex_preventable_infections']['result_with_preventable_infections_costs_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Annual Cost - Label'),
+      '#attributes' => [
+        'placeholder' => $this->t('Annual Cost'),
+      ],
+      '#default_value' => !empty($values['result_with_preventable_infections_costs_label']) ? $values['result_with_preventable_infections_costs_label'] : $config['result_with_preventable_infections_costs_label'],
+    ];
 
 
 
+*/
     return parent::buildForm($form, $form_state);
   }
 
@@ -334,9 +581,7 @@ class CalculatorResultContentForm extends ConfigFormBase
    */
   public function validateForm(array &$form, FormStateInterface $form_state)
   {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
+
     parent::validateForm($form, $form_state);
   }
 
@@ -345,9 +590,13 @@ class CalculatorResultContentForm extends ConfigFormBase
    */
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    $this->config('verathon_bflex_calculator.settings')
-      ->set('example', $form_state->getValue('example'))
-      ->save();
+
+    // print '<pre>';
+    // print_r($form_state->getValues());
+    // die;
+    // $this->config('verathon_bflex_calculator.settings')
+    //   ->set('example', $form_state->getValue('example'))
+    //   ->save();
     parent::submitForm($form, $form_state);
   }
 }
