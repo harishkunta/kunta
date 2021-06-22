d152c7554fd3f40b4fdc35f255b9b457002e1d9b
Sai Sumanth Goud Parakala <saisumanth.p@tadigital.com>
2021-06-22 22:47:11 +0530

    Adding final css and js files for static image hotspot after client confirmation

--- docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
+++ docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
@@ -3,14 +3,14 @@
         $('.h5p-image-hotspot').on('click', function() {
             if(parseInt($(this)[0].style.left) > 50) {
                 var interval = setInterval(function () { 
-                    var popUpLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("left");
+                    var popUpObj = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup"), popUpLeft = popUpObj.css("left");
                     if( popUpLeft == '0px' || popUpLeft == '0') {
                         clearInterval(interval);
                         let popUpPointerLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("left");
-                        let popUpWidth = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").width();
+                        let popUpWidth = popUpObj.width();
                         let value = parseInt(popUpPointerLeft) - popUpWidth;
-                        $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("transition", 'none');
-                        $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("left", value + 'px');
+                        popUpObj.css("transition", 'none');
+                        popUpObj.css("left", value + 'px');
                     }
                  }, 1);
             }
