1c4819c8de19e9418890ec7e282ca4f2dd645842
Divesh Kumar <divesh.kumar@tadigital.com>
2021-06-23 15:07:31 +0530

    Resolving merge conflicts.

--- docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
+++ docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
@@ -7,30 +7,13 @@
                     if (popUpLeft == '0px' || popUpLeft == '0') {
                         clearInterval(interval);
                         let popUpPointerLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("left");
-                        let popUpWidth = popUpObj.width();
+                        let popUpWidth = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").width();
                         let value = parseInt(popUpPointerLeft) - popUpWidth;
-                        popUpObj.css("transition", 'none');
-                        popUpObj.css("left", value + 'px');
+                        $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("transition", 'none');
+                        $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("left", value + 'px');
                     }
                 }, 1);
             }
-
-            // else {
-            //     var interval2 = setInterval(function() {
-            //             console.log("hi");
-            //             var popUpLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("left");
-            //             if(popUpLeft != undefined) {
-            //                 clearInterval(interval2);
-            //                 var popUptop = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("top");
-            //                 console.log(popUptop);
-            //                     $(".h5p-image-hotspot-popup").css("flex-direction", 'row');
-            //                     $(".h5p-image-hotspot-popup").css("left", parseInt(popUpLeft) - 50 + 'px');
-            //                     $(".h5p-image-hotspot-popup").css("top", popUptop);
-            //                     console.log($(".h5p-image-hotspot-popup").css('top'));
-            //             }
-            //         }, 1);
-            // }
-
         });
     });
 })(H5P.jQuery);
