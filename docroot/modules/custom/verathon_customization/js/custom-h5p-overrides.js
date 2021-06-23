bc2af5724f501a07e804b266a4b144abb44479cc
Divesh Kumar <divesh.kumar@tadigital.com>
2021-06-23 15:06:29 +0530

    Resolving merge conflicts.

--- docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
+++ docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
@@ -1,10 +1,10 @@
 (function ($) {
-    $(document).ready(function() {
-        $('.h5p-image-hotspot').on('click', function() {
-            if(parseInt($(this)[0].style.left) > 50) {
-                var interval = setInterval(function () { 
-                    var popUpObj = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup"), popUpLeft = popUpObj.css("left");
-                    if( popUpLeft == '0px' || popUpLeft == '0') {
+    $(document).ready(function () {
+        $('.h5p-image-hotspot').on('click', function () {
+            if (parseInt($(this)[0].style.left) > 50) {
+                var interval = setInterval(function () {
+                    var popUpLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup").css("left");
+                    if (popUpLeft == '0px' || popUpLeft == '0') {
                         clearInterval(interval);
                         let popUpPointerLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("left");
                         let popUpWidth = popUpObj.width();
@@ -12,10 +12,27 @@
                         popUpObj.css("transition", 'none');
                         popUpObj.css("left", value + 'px');
                     }
-                 }, 1);
+                }, 1);
             }
+
+            // else {
+            //     var interval2 = setInterval(function() {
+            //             console.log("hi");
+            //             var popUpLeft = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("left");
+            //             if(popUpLeft != undefined) {
+            //                 clearInterval(interval2);
+            //                 var popUptop = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup-pointer").css("top");
+            //                 console.log(popUptop);
+            //                     $(".h5p-image-hotspot-popup").css("flex-direction", 'row');
+            //                     $(".h5p-image-hotspot-popup").css("left", parseInt(popUpLeft) - 50 + 'px');
+            //                     $(".h5p-image-hotspot-popup").css("top", popUptop);
+            //                     console.log($(".h5p-image-hotspot-popup").css('top'));
+            //             }
+            //         }, 1);
+            // }
+
         });
-     });
+    });
 })(H5P.jQuery);
 
 
