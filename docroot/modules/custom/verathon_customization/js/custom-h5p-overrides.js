68cc8290578483a337020118b162bf147f95b548
Sai Sumanth Goud Parakala <saisumanth.p@tadigital.com>
2021-07-05 13:08:51 +0530

    removed the actions bar and also fixed font size and color issues

--- docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
+++ docroot/modules/custom/verathon_customization/js/custom-h5p-overrides.js
@@ -1,7 +1,14 @@
 (function ($) {
     $(document).ready(function() {
-        $('.h5p-image-hotspot').on('click', function() {
-            if(parseInt($(this)[0].style.left) > 50) {
+        /* 
+            Event listener for hotspot on click
+        */
+        $('.h5p-image-hotspot').on('click', function() {   
+            /* 
+                condition to handle width of popup if the hotspot clicked 
+                is at the outer half of the image
+            */      
+            if(parseInt($(this)[0].style.left) > 50) {               
                 var interval = setInterval(function () { 
                     var popUpObj = $(".h5p-image-hotspots-overlay.visible .h5p-image-hotspot-popup"), popUpLeft = popUpObj.css("left");
                     if( popUpLeft == '0px' || popUpLeft == '0') {
