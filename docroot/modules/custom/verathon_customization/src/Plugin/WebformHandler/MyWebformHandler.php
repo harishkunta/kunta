65af6cf04070ae0c5d99ef3605680dde90611619
Harsh Kharbanda <harsh.kharbanda@TECH-LP-1717.local>
2021-07-19 21:33:09 +0530

    added webform handler for pdf file download

---
+++ docroot/modules/custom/verathon_customization/src/Plugin/WebformHandler/MyWebformHandler.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\verathon_customization\Plugin\WebformHandler;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\Utility\WebformElementHelper;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+/**
+ * Create a PDF file link.
+ *
+ * @WebformHandler(
+ *   id = "pdf_file",
+ *   label = @Translation("PDF file download link."),
+ *   category = @Translation("External"),
+ *   description = @Translation("Allows Webform settings to be overridden based on submission data."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_OPTIONAL,
+ *   tokens = TRUE,
+ * )
+ */
+class MyWebformHandler extends WebformHandlerBase {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Contains the configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->httpClient = $container->get('http_client');
+    $instance->configFactory = $container->get('config.factory');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
+    global $base_url;
+
+    try {
+      // Getting config from the form settings for URL.
+      $config = $this->configFactory->get('verathon_bflex_calculator.settings')->get();
+      // Get an array of the values from the submission.
+      $values = $webform_submission->getData();
+      $file_path = pathinfo($values['file_path']);
+      $options = [
+        'attributes' => [
+          'target' => '_blank',
+          'class' => ['download']
+        ]
+      ];
+      // Forming the default URL for pardot form handler.
+      $download_file_link = Link::fromTextAndUrl($file_path['basename'], Url::fromUri($base_url . $values['file_path'], $options))->toString();
+      $webform = $webform_submission->getWebform();
+      $webform->setSetting('confirmation_message', t('Here is your file: @title', ['@title' => $download_file_link]));
+      // Forming the default URL for pardot form handler.
+      // $url = !empty($config['bflex_pardot_url']) ? $config['bflex_pardot_url'] : 'http://www2.verathon.com/l/708283/2021-07-02/297dgm';
+      // // HTTP post call to submit data to pardot endpoint.
+      // $response = $instance->httpClient->post($url, [
+      //   'verify' => true,
+      //   'form_params' => $values,
+      //   'headers' => [
+      //     'Content-type' => 'application/x-www-form-urlencoded',
+      //   ],
+      // ])->getBody()->getContents();
+      // // Logging the information.
+      // \Drupal::logger('verathon_customization_webform')->notice(print_r($response, 1));
+    } catch (\Exception $e) {
+      \Drupal::logger('verathon_customization_webform')->error($e->getMessage());
+    }
+  }
+}
